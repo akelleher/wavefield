9232ec1b32e860fa6b9f1dc35a42720f8e2b7530
timcast725 <castit@rpi.edu>
2016-11-27 20:26:38 -0500

    Updated Source Code

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,11 +3,12 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-class MainContentComponent   : public AudioAppComponent
+class MainContentComponent   : public AudioAppComponent,
+                               public Button::Listener 
 {
 public:
     //==============================================================================
-    MainContentComponent()
+    MainContentComponent() : fftObj(5,false)
     {
         levelSlider.setRange (0.0, 0.25);
         levelSlider.setTextBoxStyle (Slider::TextBoxRight, false, 100, 20);
@@ -16,8 +17,26 @@ public:
         addAndMakeVisible (levelSlider);
         addAndMakeVisible (levelLabel);
         
-        setSize (600, 100);
-        setAudioChannels (2, 2);
+        XSlider.setRange (-5.0, 5.0);
+        XSlider.setTextBoxStyle (Slider::TextBoxRight, false, 100, 20);
+        XLabel.setText ("X Position", dontSendNotification);
+        
+        addAndMakeVisible (XSlider);
+        addAndMakeVisible (XLabel);
+        
+        YSlider.setRange (-5.0, 5.0);
+        YSlider.setTextBoxStyle (Slider::TextBoxRight, false, 100, 20);
+        YLabel.setText ("Y Position", dontSendNotification);
+        
+        addAndMakeVisible (YSlider);
+        addAndMakeVisible (YLabel);
+        
+        addAndMakeVisible (submitButton);
+        submitButton.setButtonText ("Set Position");
+        submitButton.addListener (this);
+        
+        setSize (600, 200);
+        setAudioChannels (2, 62);
     }
 
     ~MainContentComponent()
@@ -31,6 +50,7 @@ public:
 
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
     {
+        std::cout << bufferToFill.numSamples << std::endl;
         AudioIODevice* device = deviceManager.getCurrentAudioDevice();
         const BigInteger activeInputChannels = device->getActiveInputChannels();
         const BigInteger activeOutputChannels = device->getActiveOutputChannels();
@@ -60,7 +80,7 @@ public:
                     float* outBuffer = bufferToFill.buffer->getWritePointer (channel, bufferToFill.startSample);
                     
                     for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
-                        outBuffer[sample] = inBuffer[sample] * random.nextFloat() * level;
+                        outBuffer[sample] = inBuffer[sample] * level;// * random.nextFloat();
                 }
             }
         }
@@ -74,12 +94,34 @@ public:
     {
         levelLabel.setBounds (10, 10, 90, 20);
         levelSlider.setBounds (100, 10, getWidth() - 110, 20);
+        
+        XLabel.setBounds (10, 70, 90, 20);
+        XSlider.setBounds (100, 70, getWidth() - 110, 20);
+        
+        YLabel.setBounds (10, 110, 90, 20);
+        YSlider.setBounds (100, 110, getWidth() - 110, 20);
+        
+        submitButton.setBounds (10, 150, getWidth() - 20, 40);
+    }
+    
+    void buttonClicked (Button* button) override
+    {
+        const float xPos = (float) XSlider.getValue();
+        const float yPos = (float) YSlider.getValue();
+        
+        /* Call Matlab WFS function, get new impulses */
     }
 
 private:
     Random random;
     Slider levelSlider;
     Label levelLabel;
+    Slider XSlider;
+    Label XLabel;
+    Slider YSlider;
+    Label YLabel;
+    FFT fftObj;
+    TextButton submitButton;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
